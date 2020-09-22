6f55feade0796ba9adf28e44cb5df28900415343
Derek Smale <dereksmale2@gmail.com>
2020-09-22 09:59:07 -0400

    version 2, no css yet

--- main.js
+++ main.js
@@ -4,19 +4,19 @@ let template = ("")
 //identify//
 console.log(customer)
 //run code//
-for (let i = 0; i < results.length; i++) {
+for (let customer of customers) {
     console.log(results)
     //run code//
     template += `
     <div class="container">
-    <img src=${results[i].picture.large}>
-    <h3>${results[i].name.first} {results[i].name.last}</h3>
+    <img src=${customer.picture.large}>
+    <h3>${customer.name.first} ${customer.name.last}</h3>
     <hr>
-    <p class="email">${results[i].email}</p>
-    <p class="street">${results[i].location.street}</p>
-    <p class="address">${results[i].location.city}. ${results[i].location.state}. ${results[i].location.postcode}</p>
-    <p class="dob">${results[i].dob}</p>
-    <p class="member">${results[i].registered}</p>
+    <p class="email">${customer.email}</p>
+    <p class="street">${customer.location.street}</p>
+    <p class="address">${customer.location.city} ${customer.location.state} ${customer.location.postcode}</p>
+    <p class="dob">${customer.dob}</p>
+    <p class="member">${customer.registered}</p>
     </div> `
     console.log(template)
     //run code//
